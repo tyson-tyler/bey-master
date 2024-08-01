d187c3caa95ef6f8fdedaafb4eb8e52dbe5504ac
tyson-tyler <benword25@gmail.com>
2024-08-01 13:32:40 +0530

    afd

--- my-project/src/api/user-resume/controllers/user-resume.ts
+++
@@ -1,5 +0,0 @@
-// src/api/user-resume/controllers/user-resume.ts
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController("api::user-resume.user-resume");
--- my-project/src/api/user-resume/routes/user-resume.ts
+++
@@ -1,5 +0,0 @@
-// src/api/user-resume/routes/user-resume.ts
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreRouter("api::user-resume.user-resume");
