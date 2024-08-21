12e480c73ee94e7d3fc34fd0bfe65b7f4e48be3b
tyson-tyler <benword25@gmail.com>
2024-08-21 11:25:25 +0530

    afds

--- lib/act/user.actions.ts
+++ lib/act/user.actions.ts
@@ -3,7 +3,6 @@
 import { revalidatePath } from "next/cache";
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
-import { handleError } from "@/utils/utils";
 
 // CREATE
 export async function createUser(user: CreateUserParams) {
@@ -14,7 +13,7 @@ export async function createUser(user: CreateUserParams) {
 
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
 }
 
@@ -29,7 +28,7 @@ export async function getUserById(userId: string) {
 
     return JSON.parse(JSON.stringify(user));
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
 }
 
@@ -46,7 +45,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
 }
 
@@ -66,7 +65,7 @@ export async function deleteUser(clerkId: string) {
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
 }
 
@@ -85,6 +84,6 @@ export async function updateCredits(userId: string, creditFee: number) {
 
     return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
 }
